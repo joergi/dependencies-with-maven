ccd9102e3cb0593c0e49c7c9e3899ca7ce087320
joergi <1439809+joergi@users.noreply.github.com>
2024-01-16 00:37:56 +0100

    first commit with maven

---
+++ src/main/kotlin/io/joergi/demopom/DemopomApplication.kt
@@ -0,0 +1,12 @@
+package io.joergi.demopom
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemopomApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemopomApplication>(*args)
+	println("hello world from maven")
+}
---
+++ src/test/kotlin/io/joergi/demopom/DemopomApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.joergi.demopom
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemopomApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
